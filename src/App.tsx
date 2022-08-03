63264d68d5c70a7ca6e27fe2ed9d53e63fa57bda
te6-in <56245920+te6-in@users.noreply.github.com>
2022-08-03 06:55:41 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+	display: flex;
+	background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const animation = keyframes`
+	0% {
+		transform: rotate(0deg);
+		border-radius: 0px;
+	}
+	50% {
+		transform: rotate(360deg);
+		border-radius: 50%;
+	}
+	100% {
+		transform: rotate(0deg);
+		border-radius: 0px;
+	}
+`;
+
+const Title = styled.h1`
+	color: ${(props) => props.theme.textColor};
+`;
+
+const Emoji = styled.span`
+	font-size: 72px;
+`;
+
+const Box = styled.div`
+	height: 200px;
+	width: 200px;
+	background-color: ${(props) => props.theme.boxColor};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	animation: ${animation} 2s linear infinite;
+
+	&:hover {
+		background-color: green;
+	}
+
+	${Emoji} {
+		&:hover {
+			font-size: 100px;
+		}
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Title>This is a Title Component!</Title>
+			<Box>
+				<Emoji>😸</Emoji>
+			</Box>
+			<Emoji>🧢</Emoji>
+		</Wrapper>
+	);
 }
 
 export default App;
